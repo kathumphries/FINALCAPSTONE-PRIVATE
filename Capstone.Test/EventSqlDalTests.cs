1620303f681905c24f80ed479f96900b26facf63
Abby Bougher <abbybougher@gmail.com>
2019-04-11 22:25:40 -0400

    Podcast Model, PodcastDal, EventDalTests

--- Capstone.Test/EventSqlDalTests.cs
+++ Capstone.Test/EventSqlDalTests.cs
@@ -37,26 +37,26 @@ namespace Capstone.Test
                 podcastEventCount = (int)command.ExecuteScalar();
 
                 /*Create new Event for testing*/
-
-                command = new SqlCommand("INSERT INTO Event(beginning, ending, coverPhoto, descriptionCopy, ticketLevel, upsaleCopy, isFinalized, eventName)" +
-                    "VALUES ('11 / 15 / 2016 03: 39 pm', '4 / 10 / 2019 10: 39 pm', 'catCount@johnfoulton.com', 'Enter Copy Here', 'dogCount@johnfoulton.com', 'VIP', 'All the cool kids are doing it!', 1, 'Pet Count = 17'); SELECT CAST(SCOPE_IDENTITY() as int);", connection);
-                podcastID = (int)command.ExecuteScalar();
-                userID = (int)command.ExecuteScalar();
-                genreID = (int)command.ExecuteScalar();
-
                 command = new SqlCommand("INSERT INTO Podcast(hosting, url, title, description, originalRelease, runTime, releaseFreqency, averageLength, numOfEpisodes, numOfDownloads, measurementPlatform, demographics, affiliations, broadcastCity, broadcastState, inOhio, isSponsored)" +
                     "VALUES ('GoDaddy.com', 'petCount.com', 'Pet Count = 17', 'Animals are better than people', '1 / 14 / 2019 09: 00 am', 3, 'daily', 7, 70, 0, 'Job Offers', 'Eclectic', 'Rev1 Ventures', 'VIP', 'Columbus', 'Ohio', 1, 1); SELECT CAST(SCOPE_IDENTITY() as int);", connection);
                 podcastID = (int)command.ExecuteScalar();
-                userID = (int)command.ExecuteScalar();
-                genreID = (int)command.ExecuteScalar();
 
                 command = new SqlCommand("INSERT INTO Venue(displayName, roomName, buildingName, address1, address2, city, state, zipCode, phoneNumber, additionalInfo, parkingInfo, isVisible)" +
                     "VALUES ('', 'Main', 'Southern Theatre', '21 E. Main Street', '', 'Columbus', 'Ohio', '43215', '(614) 469-0939', '', 'Columbus Commons Parking Garage & Ohio Statehouse Underground Parking Garage', 1); SELECT CAST(SCOPE_IDENTITY() as int);", connection);
                 venueID = (int)command.ExecuteScalar();
 
-                command = new SqlCommand("INSERT INTO User(name, email, isAdmin, isProducer)" +
-                    "VALUES ('John Fulton', 'john@techelevator.com', 1, 1); SELECT CAST(SCOPE_IDENTITY() as int);", connection);
-                userID = (int)command.ExecuteScalar();
+                command = new SqlCommand("INSERT INTO Event(podcastID, venueID, beginning, ending, coverPhoto, descriptionCopy, ticketLevel, upsaleCopy, isFinalized, eventName)" +
+                    "VALUES (@podcastID, @venueID, '11 / 15 / 2016 03: 39 pm', '4 / 10 / 2019 10: 39 pm', 'catCount@johnfoulton.com', 'Enter Copy Here', 'dogCount@johnfoulton.com', 'VIP', 'All the cool kids are doing it!', 1, 'Pet Count = 17'); SELECT CAST(SCOPE_IDENTITY() as int);", connection);
+
+                command.Parameters.AddWithValue("@podcastID", podcastID);
+                command.Parameters.AddWithValue("@venueID", venueID);
+
+                int eventID = (int)command.ExecuteScalar();
+               
+
+                
+
+
             }
         }
 
@@ -100,4 +100,7 @@ namespace Capstone.Test
 
 }
 
-
+                //Create User
+                //command = new SqlCommand("INSERT INTO User(name, email, isAdmin, isProducer)" +
+                //    "VALUES ('John Fulton', 'john@techelevator.com', 1, 1); SELECT CAST(SCOPE_IDENTITY() as int);", connection);
+                //userID = (int)command.ExecuteScalar();
